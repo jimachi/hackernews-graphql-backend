528cd45fde67f0b86fd48b4bf02d1c023cdd1054
Yuichiro Iijima <jimaichiro@gmail.com>
2022-12-20 23:14:39 +0900

    refactor: server側でprismaを実行させる

--- src/server.js
+++ src/server.js
@@ -2,41 +2,39 @@ const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-// HackerNewsの１つ１つの投稿
-const links = [
-  {
-    id: "link-0",
-    description: "GraphQLチュートリアル",
-    url: "http://example.com",
-  },
-];
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 // Resolver関数
 // 定義した型に対して値を入力する
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
 
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
-    }
-  }
+      return newLink
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({url}) => console.log(`${url}でサーバーを起動中...`));
