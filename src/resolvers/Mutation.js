53f2ce0453c33202e9a3b4e73ad04df0930bf377
Yuichiro Iijima <jimaichiro@gmail.com>
2022-12-21 00:42:07 +0900

    feat: ログイン処理の追加

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,53 @@
+const { prisma } = require('@prisma/client');
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
+
+const APP_SECRET = "Graphql";
+
+// ユーザー新規登録のリゾルバ
+const signUp = async (parent, args, context) => {
+  // passwordの設定
+  const password = await bcrypt.hash(args.password, 10);
+
+  // ユーザーの新規作成
+  const user = await context.prisma.user.create({
+    data: {
+      ...args,
+      password,
+    }
+  });
+
+  const token = jwt.sign({ userId: user.id, APP_SECRET });
+
+  return {
+    token,
+    user,
+  }
+};
+
+// user login
+const login = async (parent, args, context) => {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  })
+
+  if (!user) {
+    throw new Error("ユーザーが存在しません");
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if(!valid) {
+    throw new Error("無効なパスワードです");
+  }
+
+  const token = jwt.sign({ userId: user.id, APP_SECRET });
+  return {
+    token,
+    user,
+  }
+};
+
+modules.exports = {
+  signUp,
+  login,
+};
