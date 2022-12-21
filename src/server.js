65cc9fe4190c2ff4f5e4c240fefb32c717579d8b
Yuichiro Iijima <jimaichiro@gmail.com>
2022-12-22 01:28:32 +0900

    refactor: serverのリゾルバをきれいにまとめる

--- src/server.js
+++ src/server.js
@@ -5,29 +5,20 @@ const path = require("path");
 const { PrismaClient } = require('@prisma/client');
 const { getUserId } = require("./utils");
 
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Link = require('./resolvers/Link');
+const User = require('./resolvers/User');
+
 const prisma = new PrismaClient();
 
 // Resolver関数
 // 定義した型に対して値を入力する
 const resolvers = {
-  Query: {
-    info: () => "HackerNewsクローン",
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    },
-  },
-  Mutation: {
-    post: (parent, args, context) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-
-      return newLink
-    },
-  },
+  Query,
+  Mutation,
+  Link,
+  User
 };
 
 const server = new ApolloServer({
