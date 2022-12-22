ad783a8d96356f126bff44199967884ec38c0b03
Yuichiro Iijima <jimaichiro@gmail.com>
2022-12-23 01:45:10 +0900

    feat: voteのリゾルバの実装

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -65,8 +65,37 @@ const post = async (parent, args, context) => {
   return newLink;
 };
 
+const vote = async (parent, args, context) => {
+  const { userId } = context;
+
+  const vote = context.prisma.vote.findUnique({
+    where: {
+      linkId_userId: {
+        linkId: Number(args.linkId),
+        userId: userId,
+      }
+    }
+  });
+
+  if (Boolean(vote)) {
+    throw new Error(`既に投票されています: ${args.linkId}`);
+  }
+
+  const newVote = context.prisma.vote.create({
+    data: {
+      user: { connect: { id: userId }},
+      link: { connect: { id: Number(args.linkId) }},
+    }
+  });
+
+  context.pubsub.publish("NEW_VOTE", newVote);
+
+  return newVote;
+};
+
 module.exports = {
   signUp,
   login,
   post,
+  vote
 };
